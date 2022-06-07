a926718f0f037596f620ab4cdc20b002d909c49e
Tomiwa_dev <adewoletomiwa@gmail.com>
2022-06-07 13:22:38 -0700

    corrections

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet_x10: Entry point
+ * print_alphabet_x10 - Entry point
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
 	char c;
-	int i = 10;
+	int i = 0;
 
 	while (i < 10)
 	{
 		_putchar(c);
-		i--;
+		i++;
 	}
 	_putchar('\n');
 }
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * _islower - entry point
+ * _islower - checks for lower case
  * Return: 1, 0
  */
 int _islower(int c)
