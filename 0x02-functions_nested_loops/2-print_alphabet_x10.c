fafda11cb3df54c2966338d8644c334039a57362
Tomiwa_dev <adewoletomiwa@gmail.com>
2022-06-07 14:22:23 -0700

    corrections

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,15 @@ void print_alphabet_x10(void)
 
 	while (i < 10)
 	{
-		_putchar(c);
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
+
 }
 
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
-/*
+/**
  * _islower - checks for lower case
+ * @c : character to check the case
  * Return: 1, 0
  */
 int _islower(int c)
