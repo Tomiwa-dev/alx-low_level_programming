1f4736ec746cc195ad1e76e5f5c8dfba15461631
Tomiwa_dev <adewoletomiwa@gmail.com>
2022-06-07 12:55:23 -0700

    submission 2, 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10: Entry point
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 10;
+
+	while (i < 10)
+	{
+		_putchar(c);
+		i--;
+	}
+	_putchar('\n')
+}
+
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/*
+ * _islower - entry point
+ * Return: 1, 0
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
