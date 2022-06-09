8e11ee32908cc40b10ba97ab8f9c329d6aa747c8
Tomiwa_dev <adewoletomiwa@gmail.com>
2022-06-09 02:39:34 -0700

    corrections

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,10 +3,10 @@
 
  /**
  * prints_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
+ * Description: left in the year, taking leap years into account
  * @month: month in number format
- * @day: day of month 
- * @year: year
+ * @day: day of month
+ * * @year: year
  * Return: void
  */
 
