78aa96e4137e6658318181983fa8ba4bab138b37
Tomiwa_dev <adewoletomiwa@gmail.com>
2022-06-13 16:56:10 -0700

    corrections

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,16 +7,16 @@
 void puts_half(char *str)
 {
 	int len = 0;
-	int i;
+	int i, n;
 
 	while (str[len])
 		len++;
 	if (len % 2 == 0)
-		i = len / 2;
+		n = len / 2;
 	else
-		i = (len + 1) / 2;
+		n = (len + 1) / 2;
 
-	for (i; i < len; i++)
+	for (i = n; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
