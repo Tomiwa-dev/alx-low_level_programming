bf116ee88faa6f7c7f5f9ad309b30fc54671b686
Tomiwa_dev <adewoletomiwa@gmail.com>
2022-06-22 13:16:42 -0700

    submission 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - a function that prints a string
+ * @s: string
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+		_putchar("\n");
+}
