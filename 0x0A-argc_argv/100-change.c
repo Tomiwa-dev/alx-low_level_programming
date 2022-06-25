052ce29a3fdc54c891347ff8401eec3eabc8fcb8
Tomiwa_dev <adewoletomiwa@gmail.com>
2022-06-25 08:26:36 -0700

    betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,44 +12,28 @@ int main(int argc, char **argv)
 	int change, cent = 0;
 
 	if (argc == 2)
-	{
 		cent = atoi(argv[1]);
 
 		if (cent < 0)
-		{
 			printf("%d\n", 0);
 			return (0);
-		}
 		if (cent % 25 >= 0)
-		{
 			change += cent / 25;
 			cent = cent % 25;
-		}
 		if (cent % 10 >= 0)
-		{
 			change += cent / 10;
 			cent = cent % 10;
-		}
 		if (cent % 5 >= 0)
-		{
 			change += cent / 5;
 			cent = cent % 5;
-		}
 		if (cent % 2 >= 0)
-		{
 			change += cent / 2;
 			cent = cent % 2;
-		}
 		if (cent % 1 >= 0)
-		{
 			change += cent;
-		}
 		printf("%d\n", change);
 		return (0);
-	}
 	else
-	{
 		printf("Error\n");
 		return (1);
-	}
 }
